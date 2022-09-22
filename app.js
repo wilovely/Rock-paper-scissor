775e20be26f9689e0c85a4e7dd3d67dc91905619
wi <wiwimon333@gmail.com>
2022-09-22 21:35:22 +0800

    added player computer and draw score and created function game that can play 5 rounds

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+let player = 0;
+let computer = 0;
+let draw = 0;
+
 //create a random choice function for rock paper scissor 
 function getComputerChoice() {
     let num = Math.round(Math.random() * 2 + 1);
-    console.log(num);
     var choose = "";
     switch (num) {
         case 1: var choose = "rock";
@@ -18,22 +21,35 @@ function getComputerChoice() {
 
 //function that plays a single round of Rock Paper Scissors. The function have two parameters
 function playRound(playerSelection, computerSelection) {
-    console.log("player chose " + playerSelection + " Computer chose " + computerSelection);
+    console.log("You choose " + playerSelection + " Computer choose " + computerSelection);
 
-    if(playerSelection==="paper" && computerSelection==="rock"){
+    if (playerSelection === "paper" && computerSelection === "rock") {
         console.log("you win!");
-    }else if(playerSelection==="scissor"&& computerSelection==="paper"){
+        player = ++player;
+    } else if (playerSelection === "scissor" && computerSelection === "paper") {
         console.log("you win!");
-    }else if(playerSelection==="rock"&& computerSelection==="scissor"){
+        player = ++player;
+    } else if (playerSelection === "rock" && computerSelection === "scissor") {
         console.log("you win!");
-    }else if(playerSelection===computerSelection){
+        player = ++player;
+    } else if (playerSelection === computerSelection) {
         console.log("its a draw!");
-    }else{
+        draw = ++draw;
+    } else {
         console.log("you lose!");
+        computer = ++computer;
     }
 }
 
-//create a prompt for player to enter and made input to case insensitive
-const playerSelection = prompt("enter rock paper or scissor").toLowerCase();
-const computerSelection = getComputerChoice();
-playRound(playerSelection, computerSelection);
+function game() {
+
+    for (let i = 0; i < 5; i++) {
+        //created a prompt for player to enter and made input to case insensitive
+        const playerSelection = prompt("enter rock paper or scissor").toLowerCase();
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+    }
+    //show score numbers
+    console.log(" player score: " + player + "\n computer score: " + computer + "\n draw: " + draw + "\n you lose!");
+}
+game();
