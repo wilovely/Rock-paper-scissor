2599f3ae41f9db732028fd3d9e6014c611a2130f
wi <wiwimon333@gmail.com>
2022-09-22 19:26:06 +0800

    added a console.log showing player choice and computer choice and added prompt for player selection

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-getComputerChoice();
-
 //create a random choice function for rock paper scissor 
 function getComputerChoice() {
     let num = Math.round(Math.random() * 2 + 1);
@@ -15,5 +13,17 @@ function getComputerChoice() {
         case 3: var choose = "scissor";
             break;
     }
-    return console.log(`computer chose ${choose}`);
+    return choose;
 }
+
+//Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters
+function playRound(playerSelection, computerSelection) {
+    console.log("player chose " + playerSelection + " Computer chose " + computerSelection);
+
+
+}
+
+//create a prompt for player to enter
+const playerSelection = prompt("enter rock paper or scissor");
+const computerSelection = getComputerChoice();
+playRound(playerSelection, computerSelection);
